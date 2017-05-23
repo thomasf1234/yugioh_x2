3d3c61b074df2b85a831667e0b876d437234ec79
Thomas F <thomas.fisher@footballpools.com>
2017-05-23 17:28:54 +0100

    [#0] TF : adding deposit and withdrw methods on User for dp

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,29 @@ module YugiohX2
     end
 
     has_one :session
+
+    def deposit(amount)
+      if !YugiohX2Lib::Utils.positive_integer?(amount)
+        raise ArgumentError.new("amount must be a positive integer")
+      else
+        new_dp = self.dp + amount
+        update_attribute(:dp, new_dp)
+        amount
+      end
+    end
+
+    def withdraw(amount)
+      if !YugiohX2Lib::Utils.positive_integer?(amount)
+        raise ArgumentError.new("amount must be a positive integer")
+      else
+        if amount > self.dp
+          raise ArgumentError.new("There is not enough dp to make the requested withdrawl")
+        else
+          new_dp = self.dp - amount
+          update_attribute(:dp, new_dp)
+          amount
+        end
+      end
+    end
   end
 end
--- spec/app/models/user_spec.rb
+++ spec/app/models/user_spec.rb
@@ -50,6 +50,66 @@ module YugiohX2Spec
           expect(user.dp).to eq(0)
         end
       end
+
+      describe "#deposit" do
+        let(:user) { YugiohX2::User.create(username: "TestUser", encrypted_password: "{SHA256}some_encrypted_password") }
+
+        context 'invalid deposit amount' do
+          [0, -1, 0.5, '1'].each do |invalid_deposit|
+            it 'raises argument error' do
+              expect { user.deposit(invalid_deposit) }.to raise_error(ArgumentError, "amount must be a positive integer")
+            end
+          end
+        end
+
+        context 'valid deposit amount' do
+          it 'raises the users dp' do
+            user.deposit(1)
+            expect(user.dp).to eq(1)
+
+            user.deposit(5)
+            expect(user.dp).to eq(6)
+
+            user.deposit(50)
+            expect(user.dp).to eq(56)
+          end
+        end
+      end
+
+      describe "#withdraw" do
+        let(:user) { YugiohX2::User.create(username: "TestUser", encrypted_password: "{SHA256}some_encrypted_password", dp: 50) }
+
+        context 'invalid withdrawal amount' do
+          [0, -1, 0.5, '1'].each do |invalid_withdrawal|
+            it 'raises argument error' do
+              expect { user.withdraw(invalid_withdrawal) }.to raise_error(ArgumentError, "amount must be a positive integer")
+            end
+          end
+        end
+
+        context 'valid withdraw amount' do
+          context 'not enough dp to satisfy the withdrawl' do
+            it "raises ArgumentError and does not update the users dp" do
+              expect { user.withdraw(51) }.to raise_error(ArgumentError, "There is not enough dp to make the requested withdrawl")
+              user.reload
+              expect(user.dp).to eq(50)
+            end
+          end
+
+          context 'enough dp to satisfy the withdrawal' do
+            it 'reduces the users dp' do
+              user.withdraw(1)
+              expect(user.dp).to eq(49)
+
+              user.withdraw(5)
+              expect(user.dp).to eq(44)
+
+              user.withdraw(23)
+              expect(user.dp).to eq(21)
+            end
+          end
+        end
+      end
     end
   end
 end
