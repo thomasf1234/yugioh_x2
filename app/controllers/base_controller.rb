643354e2243bf276b4764c845b25282bcaa24498
Thomas F <thomas.fisher@footballpools.com>
2017-05-29 12:06:13 +0100

    #[0] Adding the shop controller with password machine. TODO add test

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -27,14 +27,22 @@ module YugiohX2
     end
 
     def logged_in?(uuid, remote_ip)
-      Session.where(uuid: uuid, remote_ip: remote_ip)
-      session = Session.find_by(uuid: uuid, remote_ip: remote_ip)
+      session = current_session(uuid, remote_ip)
 
       if session.nil? || session.expired?
         false
       else
         true
       end
+    end
+
+    def current_user(uuid, remote_ip)
+      session = current_session(uuid, remote_ip)
+      session.user
+    end
+
+    def current_session(uuid, remote_ip)
+      Session.find_by(uuid: uuid, remote_ip: remote_ip)
     end
   end
 end
---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,44 @@
+require_relative 'base_controller'
+
+module YugiohX2
+  class ShopsController < BaseController
+    def password_machine(request)
+      if valid_params?(request.query, ['uuid', 'serial_number'])
+        uuid = request.query['uuid']
+        serial_number = request.query['serial_number']
+
+        if logged_in?(uuid, request.remote_ip)
+          user = current_user(uuid, request.remote_ip)
+
+          if user.dp > 1000
+            card = YugiohX2::Card.find_by_serial_number(serial_number)
+
+            if card.nil?
+              render({json: {message: "No card found for serial_number"}.to_json}, 422)
+            else
+              if user.user_cards.exists?(card_id: card.id)
+                user_card = user.user_cards.find_by_card_id(card.id)
+
+                ActiveRecord::Base.transaction do
+                  user_card.increment!(:count)
+                  user.withdraw(1000)
+                end
+
+                #check if successful
+                render json: {message: "User has purchased #{user_card.card.name}", card_id: user_card.card_id}.to_json
+              else
+                render({json: {message: "User does not already possess a copy"}.to_json}, 403)
+              end
+            end
+          else
+            render({json: {message: "User does not have enough dp to fulfill the request"}.to_json}, 402)
+          end
+        else
+          render({json: {message: "You are not authorized to make this request"}.to_json}, 401)
+        end
+      else
+        render({json: {message: "invalid request parameters"}.to_json}, 422)
+      end
+    end
+  end
+end
