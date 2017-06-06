9f225aef5e2154e76d71f7793f37598a11f71438
Thomas F <thomas.fisher@footballpools.com>
2017-06-06 23:56:13 +0100

    [#0] TF : adding max_limit method to ForbiddenLimitedList to query for a card (0 for forbidden, 1 for limited, 3 for unlimited etc)

--- app/models/forbidden_limited_list.rb
+++ app/models/forbidden_limited_list.rb
@@ -7,5 +7,24 @@ module YugiohX2
     def self.current
       where("effective_from < ?", Date.today).order(effective_from: :desc).first
     end
+
+    def max_limit(card)
+      forbidden_limited_list_card = forbidden_limited_list_cards.find_by_card_id(card.id)
+
+      if forbidden_limited_list_card.nil?
+        3
+      else
+        case forbidden_limited_list_card.limited_status
+          when ForbiddenLimitedListCard::LimitedStatus::FORBIDDEN
+            0
+          when ForbiddenLimitedListCard::LimitedStatus::LIMITED
+            1
+          when ForbiddenLimitedListCard::LimitedStatus::SEMI_LIMITED
+            2
+          else
+            raise "Unknown limited_status for forbidden_limited_list_card_id: #{forbidden_limited_list_card.id}"
+        end
+      end
+    end
   end
 end
