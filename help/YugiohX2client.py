d7b203bdcd72dffd246a1b70bcf9cb7586400f72
Thomas F <thomas.fisher@footballpools.com>
2018-04-17 19:29:57 +0100

    [#0] TF : WIP  adding basic python client

---
+++ help/YugiohX2client.py
@@ -0,0 +1,9 @@
+class Shape:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.description = "This shape has not been described yet"
+        self.author = "Nobody has claimed to make this shape yet"
+
+    def area(self):
