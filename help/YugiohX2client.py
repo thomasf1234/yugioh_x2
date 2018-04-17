6d6f326342a69c5be9821957de66035af0c7a7f2
Thomas F <thomas.fisher@footballpools.com>
2018-04-17 19:31:58 +0100

    [#0] TF : WIP  adding basic python client

--- help/YugiohX2client.py
+++ help/YugiohX2client.py
@@ -1,9 +1,24 @@
-class Shape:
+import requests
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.description = "This shape has not been described yet"
-        self.author = "Nobody has claimed to make this shape yet"
+class YugiohX2Exception(Exception):
+    pass
 
-    def area(self):
+class YugiohX2Client:
+    TIMEOUT = 5000
+
+    def __init__(self, server_ip="localhost", server_port="2000"):
+        self.server_ip = server_ip
+        self.server_port = server_port
+
+    def healthcheck(self):
+        __get("/healthcheck")
+
+    #private
+    def __get(self, end_point):
+        url = "%s:%i%s" % (self.server_ip,self.server_port, end_point)
+        response = requests.get(url, timeout=self.TIMEOUT)
+        if response.status_code:
+            return response.json()
+        else:
+            raise YugiohX2Exception()
+
