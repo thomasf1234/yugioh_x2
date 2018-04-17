b693f6a339b9df3fba79655ff918d2ada0085454
Thomas F <thomas.fisher@footballpools.com>
2018-04-17 21:25:23 +0100

    [#0] TF : correcting YugiohX2client.py

--- help/YugiohX2client.py
+++ help/YugiohX2client.py
@@ -11,13 +11,14 @@ class YugiohX2Client:
         self.server_port = server_port
 
     def healthcheck(self):
-        __get("/healthcheck")
+        json = self.__get("/healthcheck")
+        return json
 
     #private
     def __get(self, end_point):
-        url = "%s:%i%s" % (self.server_ip,self.server_port, end_point)
+        url = "http://%s:%i%s" % (self.server_ip,self.server_port, end_point)
         response = requests.get(url, timeout=self.TIMEOUT)
-        if response.status_code:
+        if response.status_code == 200:
             return response.json()
         else:
             raise YugiohX2Exception()
